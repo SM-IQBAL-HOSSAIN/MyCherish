ffaa4fecd9a1e47bc387c7cc11e4e8a32868a81f
SM Iqbal Hossain <smiqbal.hossain7@gmail.com>
2024-05-17 19:46:38 +0600

    frist commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+
+  // document.documentElement.querySelector("#aboutMe").addEventListener("click",displayDate()); 
+
+
+  // function displayDate() {
+  //   document.getElementById("demo4").innerHTML = "Name : SM Iqbal Hossain , Cell : 01646364414";
+  // }
+
+
+
+
+  document.getElementById("aboutMe").addEventListener("click", display);
+
+  function display() {
+    document.getElementById("demo4").textContent = "Name : SM Iqbal Hossain"+"<br>" +"e-mail : smiqbal.hossain7@gmail.com" ;
+
+  
+  }
+
+
